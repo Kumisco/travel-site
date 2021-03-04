584f757dd987a06ae95bc1d14846a8364a91bad1
Kumisco <kumndansack@gmail.com>
2021-03-04 15:29:11 +0100

    Started reveal-on-scroll.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
 if (module.hot) {
     module.hot.accept()
 }
 
-let mobileMenu = new MobileMenu
+let mobileMenu = new MobileMenu()
+let revealOnScroll = new RevealOnScroll()
