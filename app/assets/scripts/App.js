3722ada71414ce93c5f01879ee0d39f5c9ac4b5a
Kumisco <kumndansack@gmail.com>
2021-03-05 20:58:06 +0100

    Started sticky-header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import StickyHeader from './modules/StickyHeader'
 
 if (module.hot) {
     module.hot.accept()
 }
 
-let mobileMenu = new MobileMenu
+let mobileMenu = new MobileMenu()
+let stickyHeader = new StickyHeader()
