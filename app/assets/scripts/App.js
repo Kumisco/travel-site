650bf0560f404f27bf6bc5dac6f5095bc28972ed
Kumisco <kumndansack@gmail.com>
2021-03-06 03:46:52 +0100

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,8 +7,22 @@ if (module.hot) {
     module.hot.accept()
 }
 
+new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+new MobileMenu()
+let modal
 
-let mobileMenu = new MobileMenu()
-let stickyHeader = new StickyHeader()
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import('./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem."))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
